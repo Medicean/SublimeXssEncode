d568b74e5e72b60c6e394a98b9efd655b3147ce2
Medicean <Medici.Yan@Gmail.com>
2016-01-19 00:07:05 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,162 @@
 # SublimeXssEncode
 Converts characters from one encoding to another using a transformation.
+
+**Convert the region you selected or convert all characters.**
+
+Installation
+----
+1. Open the Sublime Text Packages folder
+    * OS X: ~/Library/Application Support/Sublime Text 3/Packages/
+    * Windows: %APPDATA%/Sublime Text 3/Packages/
+    * Linux: ~/.Sublime Text 3/Packages/ or ~/.config/sublime-text-3/Packages
+
+2. clone this repo
+	
+	```
+	git clone https://github.com/Medicean/SublimeXssEncode.git
+	```
+
+Commands
+----
+
+* `html_escape`
+
+	Converts characters to their HTML entity.
+	
+	**eg:**
+	
+	`a1@&` => `a1@&amp;`
+
+* `html10_encode`
+	
+	Converts characters to html entity with decimal.
+	
+	**eg:**
+	
+	`a1@&` => `&#97;&#49;&#64;&#38;`
+
+* `html16_encode`
+
+	Converts characters to html entity with hexadecimal.
+
+	**eg:**
+	
+	`a1@&` => `&#x61;&#x31;&#x40;&#x26;`
+
+* `html_unescape`
+
+	Converts html entity to characters.
+	
+	**eg:**
+	
+	`a&#97;&#x61;&amp;` => `aaa&`
+
+* `base64_encode`
+
+	Uses base64 to encode into base64
+	
+	**eg:**
+	
+	`a1@&` => `YTFAJg==`
+
+* `base64_decode`
+
+	**eg:**
+	
+	`YTFAJg==` => `a1@&`
+
+* `url_encode`
+
+	**eg:**
+	
+	`alert(/xss/);` => `alert%28/xss/%29%3B`
+
+* `url_decode`
+
+	**eg:**
+	
+	`alert%28/xss/%29%3B` => `alert(/xss/);`
+
+* `string_from_char_code`
+
+	**eg:**
+	
+	`alert(/xss/);` => `String.fromCharCode(97,108,101,114,116,40,47,120,115,115,47,41,59)`
+
+* `mysql_char`
+
+	**eg:**
+	
+	`123` => `CHAR(49,50,51)`
+	
+	You can excute the sql commands below.
+	
+	`select 123;`
+	
+	`select CHAR(49,50,51);`
+	
+* `oracle_chr`
+
+	**eg:**
+	
+	`123` => `CHR(49)||CHR(50)||CHR(51)`
+	
+	You can excute the sql commands below.
+	
+	`select 123;`
+	
+	`select CHR(49)||CHR(50)||CHR(51);`
+
+* `php_chr`
+	
+	Convert characters with function chr.
+	
+	**eg:**
+	
+	Support we have a php backdoor, and the content is `<?php @eval($_REQUEST[cmd]);?>`
+	
+	if you want to execute some commands which includes special chars, you can convert it.
+	
+	`ls -al` => `CHR(108).CHR(115).CHR(32).CHR(45).CHR(97).CHR(108)`
+	
+	now you can request the url below:
+	
+	`http://127.0.0.1/backdoor.php?cmd=system(CHR(108).CHR(115).CHR(32).CHR(45).CHR(97).CHR(108));`
+	
+* `string_to_hex`
+
+	Convert string to hexadecimal, it's more useful for sql injection.
+
+	**eg:**
+	
+	`root` => `726f6f74`
+
+	now you can excute the sql commands below.
+
+	`SELECT user from mysql.user where user='root';`
+	
+	`SELECT user from mysql.user where user=0x726f6f74;`
+
+* `hex_to_string`
+
+	**eg:**
+	
+	`726f6f74` => `root`
+
+* `unicode_decode`
+
+	**eg:**
+	
+	`测试` => `\u6d4b\u8bd5`
+
+* `unicode_encode`
+
+	**eg:**
+	
+	`\u6d4b\u8bd5` => `测试`
+
+* `md5_encode`
+
+	**eg:**
+	
+	`1` => `c4ca4238a0b923820dcc509a6f75849b`
---
+++ xssencode.py
@@ -0,0 +1,192 @@
+import sublime
+import sublime_plugin
+
+
+class XssEncodeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for region in self.view.sel():
+            if region.empty():
+                region = sublime.Region(0, self.view.size())
+            text = self.view.substr(region)
+            replacement = self.convert(text)
+            self.view.replace(edit, region, replacement)
+
+    def convert(self, source_txt):
+        return source_txt
+
+
+class HtmlUnescapeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        try:
+            from html.parser import HTMLParser
+        except:
+            from HTMLParser import HTMLParser
+        return HTMLParser().unescape(source_txt)
+
+
+class HtmlEscapeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        try:
+            import cgi
+        except:
+            return source_txt
+        return cgi.escape(source_txt)
+
+
+class Base64EncodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        try:
+            import base64
+        except:
+            return source_txt
+        return base64.b64encode(source_txt.encode(encoding="utf-8")).decode()
+
+
+class Base64DecodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        try:
+            import base64
+        except:
+            return source_txt
+        return base64.b64decode(source_txt).decode()
+
+
+class UrlEncodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        try:
+            from urllib.parse import quote
+        except:
+            from urllib import quote
+        return quote(source_txt)
+
+
+class UrlDecodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        try:
+            from urllib.parse import unquote
+        except:
+            from urllib import unquote
+        return unquote(source_txt)
+
+
+class Md5EncodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        try:
+            import hashlib
+        except:
+            return source_txt
+        return hashlib.md5(source_txt.encode(encoding="utf-8")).hexdigest()
+
+
+class Html10EncodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = ""
+        try:
+            for i in range(len(source_txt)):
+                text += "&#%s;" % ord(source_txt[i])
+            return text
+        except:
+            sublime.error_message("Can not convert to HTML10 Entities")
+
+
+class Html16EncodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = ""
+        try:
+            for i in range(len(source_txt)):
+                text += "&#x%x;" % ord(source_txt[i])
+            return text
+        except:
+            sublime.error_message("Can not convert to HTML16 Entities")
+
+
+class StringFromCharCodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = "String.fromCharCode("
+        try:
+            for i in range(len(source_txt)):
+                text += "%s," % ord(source_txt[i])
+            text = text[:-1] + ")"
+            return text
+        except:
+            sublime.error_message("Can not convert to String.fromCharCode")
+
+
+class MysqlCharCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = "CHAR("
+        try:
+            for i in range(len(source_txt)):
+                text += "%s," % str(ord(source_txt[i]))
+            text = text[:-1] + ")"
+            return text
+        except:
+            sublime.error_message("Can not convert to MysqlChar")
+
+
+class OracleChrCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = ""
+        try:
+            for i in range(len(source_txt)):
+                text += "CHR(%s)||" % str(ord(source_txt[i]))
+            text = text[:-2]
+            return text
+        except:
+            sublime.error_message("Can not convert to OracleChr")
+
+
+class PhpChrCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = ""
+        try:
+            for i in range(len(source_txt)):
+                text += "CHR(%s)." % str(ord(source_txt[i]))
+            text = text[:-1]
+            return text
+        except:
+            sublime.error_message("Can not convert to PhpChr")
+
+
+class StringToHexCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = ""
+        try:
+            import binascii
+            text += binascii.b2a_hex(source_txt.encode('utf-8')).decode()
+            return text
+        except:
+            sublime.error_message("Can not convert to StringToHex")
+
+
+class HexToStringCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = ""
+        try:
+            if source_txt.startswith('0x'):
+                source_txt = source_txt[2:]
+            import binascii
+            text += binascii.a2b_hex(source_txt).decode()
+            return text
+        except:
+            sublime.error_message("Can not convert to HexToString")
+
+
+class UnicodeDecodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = ""
+        try:
+            text = source_txt.encode().decode('unicode_escape')
+            return text
+        except:
+            sublime.error_message("Can not convert to UnicodeDecode")
+
+
+class UnicodeEncodeCommand(XssEncodeCommand):
+    def convert(self, source_txt):
+        text = ""
+        try:
+            text = source_txt.encode('unicode_escape').decode()
+            return text
+        except:
+            sublime.error_message("Can not convert to UnicodeEncode")
