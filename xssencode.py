dabbe6647d53462f9d33504b0b76a83ef6961e2d
Medicean <Medici.Yan@Gmail.com>
2017-08-01 15:48:29 +0800

    Add aaEncode/aaDecode Encode any JavaScript program to Japanese style emoticons (^_^)

--- xssencode.py
+++ xssencode.py
@@ -1,6 +1,6 @@
 import sublime
 import sublime_plugin
-__VERSION__ = '1.0.4'
+__VERSION__ = '1.0.5'
 
 
 class XssEncodeCommand(sublime_plugin.TextCommand):
@@ -435,6 +435,107 @@ class Js16DecodeCommand(XssEncodeCommand):
             return text
         except:
             sublime.error_message("Js16Decode convert failed.")
+
+
+class AaEncodeCommand(XssEncodeCommand):
+    u"""Encode any JavaScript program to Japanese style emoticons (^_^)."""
+
+    def aaencode(self, text):
+        import re
+        try:
+            text = unicode(text)
+        except:
+            pass
+        t = ""
+        b = [
+            "(c^_^o)",
+            "(ﾟΘﾟ)",
+            "((o^_^o) - (ﾟΘﾟ))",
+            "(o^_^o)",
+            "(ﾟｰﾟ)",
+            "((ﾟｰﾟ) + (ﾟΘﾟ))",
+            "((o^_^o) +(o^_^o))",
+            "((ﾟｰﾟ) + (o^_^o))",
+            "((ﾟｰﾟ) + (ﾟｰﾟ))",
+            "((ﾟｰﾟ) + (ﾟｰﾟ) + (ﾟΘﾟ))",
+            "(ﾟДﾟ) .ﾟωﾟﾉ",
+            "(ﾟДﾟ) .ﾟΘﾟﾉ",
+            "(ﾟДﾟ) ['c']",
+            "(ﾟДﾟ) .ﾟｰﾟﾉ",
+            "(ﾟДﾟ) .ﾟДﾟﾉ",
+            "(ﾟДﾟ) [ﾟΘﾟ]"
+        ]
+        r = "ﾟωﾟﾉ= /｀ｍ´）ﾉ ~┻━┻   //*´∇｀*/ ['_']; o=(ﾟｰﾟ)  =_=3; c=(ﾟΘﾟ) =(ﾟｰﾟ)-(ﾟｰﾟ); "
+        if re.search('ひだまりスケッチ×(365|３５６)\s*来週も見てくださいね[!！]', text):
+            r += "X=_=3; "
+            r += "\r\n\r\n    X / _ / X < \"来週も見てくださいね!\";\r\n\r\n"
+
+        r += "(ﾟДﾟ) =(ﾟΘﾟ)= (o^_^o)/ (o^_^o);" +\
+            "(ﾟДﾟ)={ﾟΘﾟ: '_' ,ﾟωﾟﾉ : ((ﾟωﾟﾉ==3) +'_') [ﾟΘﾟ] " + \
+            ",ﾟｰﾟﾉ :(ﾟωﾟﾉ+ '_')[o^_^o -(ﾟΘﾟ)] " +\
+            ",ﾟДﾟﾉ:((ﾟｰﾟ==3) +'_')[ﾟｰﾟ] }; (ﾟДﾟ) [ﾟΘﾟ] =((ﾟωﾟﾉ==3) +'_') [c^_^o];" +\
+            "(ﾟДﾟ) ['c'] = ((ﾟДﾟ)+'_') [ (ﾟｰﾟ)+(ﾟｰﾟ)-(ﾟΘﾟ) ];" +\
+            "(ﾟДﾟ) ['o'] = ((ﾟДﾟ)+'_') [ﾟΘﾟ];" +\
+            "(ﾟoﾟ)=(ﾟДﾟ) ['c']+(ﾟДﾟ) ['o']+(ﾟωﾟﾉ +'_')[ﾟΘﾟ]+ ((ﾟωﾟﾉ==3) +'_') [ﾟｰﾟ] + " +\
+            "((ﾟДﾟ) +'_') [(ﾟｰﾟ)+(ﾟｰﾟ)]+ ((ﾟｰﾟ==3) +'_') [ﾟΘﾟ]+" +\
+            "((ﾟｰﾟ==3) +'_') [(ﾟｰﾟ) - (ﾟΘﾟ)]+(ﾟДﾟ) ['c']+" +\
+            "((ﾟДﾟ)+'_') [(ﾟｰﾟ)+(ﾟｰﾟ)]+ (ﾟДﾟ) ['o']+" +\
+            "((ﾟｰﾟ==3) +'_') [ﾟΘﾟ];(ﾟДﾟ) ['_'] =(o^_^o) [ﾟoﾟ] [ﾟoﾟ];" +\
+            "(ﾟεﾟ)=((ﾟｰﾟ==3) +'_') [ﾟΘﾟ]+ (ﾟДﾟ) .ﾟДﾟﾉ+" +\
+            "((ﾟДﾟ)+'_') [(ﾟｰﾟ) + (ﾟｰﾟ)]+((ﾟｰﾟ==3) +'_') [o^_^o -ﾟΘﾟ]+" +\
+            "((ﾟｰﾟ==3) +'_') [ﾟΘﾟ]+ (ﾟωﾟﾉ +'_') [ﾟΘﾟ]; " +\
+            "(ﾟｰﾟ)+=(ﾟΘﾟ); (ﾟДﾟ)[ﾟεﾟ]='\\\\'; " +\
+            "(ﾟДﾟ).ﾟΘﾟﾉ=(ﾟДﾟ+ ﾟｰﾟ)[o^_^o -(ﾟΘﾟ)];" +\
+            "(oﾟｰﾟo)=(ﾟωﾟﾉ +'_')[c^_^o];" +\
+            "(ﾟДﾟ) [ﾟoﾟ]='\\\"';" +\
+            "(ﾟДﾟ) ['_'] ( (ﾟДﾟ) ['_'] (ﾟεﾟ+"
+        r += "(ﾟДﾟ)[ﾟoﾟ]+ "
+        for i in range(len(text)):
+            n = ord(text[i])
+            t = "(ﾟДﾟ)[ﾟεﾟ]+"
+            if(n <= 127):
+                nt = "%o" % n
+                for x in range(len(nt)):
+                    t += b[int(nt[x])] + "+ "
+            else:
+                nt = "%04x" % n
+                t += "(oﾟｰﾟo)+ "
+                for x in range(len(nt)):
+                    t += b[int(eval("0x%s" % nt[x]))] + "+ "
+            r += t
+        r += "(ﾟДﾟ)[ﾟoﾟ]) (ﾟΘﾟ)) ('_');"
+        return r
+
+    def convert(self, source_txt):
+        try:
+            text = self.aaencode(source_txt)
+            return text
+        except:
+            sublime.error_message("aaEncode convert failed.")
+
+
+class AaDecodeCommand(XssEncodeCommand):
+    u"""Encode any JavaScript program to Japanese style emoticons (^_^)."""
+
+    def convert(self, source_txt):
+        try:
+            evalPreamble = u"(\uFF9F\u0414\uFF9F) ['_'] ( (\uFF9F\u0414\uFF9F) ['_'] ("
+            decodePreamble = u"( (\uFF9F\u0414\uFF9F) ['_'] ("
+            evalPostamble = u") (\uFF9F\u0398\uFF9F)) ('_');"
+            decodePostamble = u") ());"
+            text = source_txt.strip()
+            if text.rfind(evalPostamble) < 0:
+                sublime.error_message("Given code is not encoded as aaencode.")
+                return source_txt
+            if text.rfind(evalPostamble) != len(text) - len(evalPostamble):
+                sublime.error_message("Given code is not encoded as aaencode.")
+                return source_txt
+            text = text.replace(evalPreamble, decodePreamble).replace(
+                evalPostamble, decodePostamble)
+            sublime.message_dialog('Decode end. Run the script you will see result.')
+            return text
+        except:
+            sublime.error_message("aaDecode convert failed.")
 
 
 class HexStripxCommand(XssEncodeCommand):
